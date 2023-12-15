8fca95c489b85118cc8199fd656a8b32986053eb
Zouraiz <84671958+zouraiz523@users.noreply.github.com>
2023-12-15 22:30:33 +0500

    Add files via upload

---
+++ LinkTree/script.js
@@ -0,0 +1,9 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["Youtubers", "Developer", "Blogger", "Designer", "Freelancer", "Developer Zouraiz"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    cursorChar: '|',
+    smartBackspace: true,
+    loop: true
+});
